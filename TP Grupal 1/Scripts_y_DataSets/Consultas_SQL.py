1847739d3c160510ae953eab622a1d1ed4aa3fd0
Fran Pucciarelli <166013213+franpuch@users.noreply.github.com>
2025-05-23 21:14:16 -0300

    Actualización 'Scripts_y_DataSets': Casi Final.

---
+++ TP Grupal 1/Scripts_y_DataSets/Consultas_SQL.py
@@ -0,0 +1,246 @@
+# Laboratorio de Datos - TP 1. 
+'''
+Integrantes -> Dorogov Cristina 
+            -> Pucciarelli Francisco Lautaro 
+            -> Salto Julian 
+               
+Consultas SQL (sobre los DataSets limpios y procesados).
+'''
+
+'''
+Si aún no leyó el 'README', por favor leerlo.
+'''
+
+import pandas as pd
+import duckdb
+from pathlib import Path
+
+'''
+Preparación de los Path a los diferentes archivos y carpeta de destino.
+'''
+
+base_path = Path(__file__).resolve().parent        # Obtener la ruta base del script actual. 
+modelo_path = base_path / 'TablasModelo'           # Ruta a la carpeta donde se encuentran los DataSets limpios. 
+consultas_path = base_path / 'ConsultasSQL'        # Ruta a la carpeta donde se van a guardar los DataSets limpios.
+consultas_path.mkdir(exist_ok=True)                # Crear la carpeta de salida para las consultas SQL. 
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Lectura de DataSets.
+'''
+
+BP = pd.read_csv(modelo_path / "bibliotecas_populares.csv")
+EE = pd.read_csv(modelo_path / "establecimientos_educativos.csv")
+dept = pd.read_csv(modelo_path / "departamentos.csv")
+EEcomunes = pd.read_csv(modelo_path / "establecimientos_educativos_comunes.csv")
+nivel_educ_dept = pd.read_csv(modelo_path / "nivel_educativo_por_departamento.csv")
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Consultas.
+'''
+
+### Consulta i.
+
+# Sumo la cantidad de niveles educativos (por separado) que hay en cada departamento.
+
+consulta_i = duckdb.query(""" 
+                          SELECT
+                            ANY_VALUE(EE.id_departamento) as id_dept, 
+                            sum(EEcomunes.jardin_maternal) AS jardin_maternal,          --Sumo la cantidad de cada nivel educativo.
+                            sum(EEcomunes.jardin_infantes) AS jardin_infantes,      
+                            sum(EEcomunes.primario) AS primarias,
+                            sum(EEcomunes.secundario) AS secundarios,
+                            ANY_VALUE (dept.nombre_departamento) AS departamento,
+                            ANY_VALUE(dept.nombre_provincia) AS Provincia
+                          FROM EEcomunes 
+                          INNER JOIN EE 
+                            ON EE.id_establecimiento = EEcomunes.id_establecimiento     --Joineo EEcomunes con EE para quedarme solo con los establecimientos comunes.
+                          INNER JOIN dept
+                            ON dept.id_departamento = EE.id_departamento                --Joineo la tabla (con la que vengo trabajando) con la tabla dept para obtener el nombre del departamento y de la provincia.
+  
+                          GROUP BY dept.id_departamento                                 --Agrupo por id_departamento.
+
+                         """) 
+dfi = consulta_i.df()  
+
+consultaSQL2 = duckdb.query(""" 
+                            SELECT 
+                              dfi.Provincia,  
+                              dfi.jardin_maternal,
+                              nivel_educ_dept.jardin_maternal AS Poblacion_maternal,
+                              dfi.jardin_infantes,
+                              nivel_educ_dept.jardin_infante AS Poblacion_infante,
+                              dfi.primarias,
+                              nivel_educ_dept.primaria AS poblacion_primaria,
+                              dfi.secundarios,
+                              nivel_educ_dept.secundaria AS poblacion_secundaria,
+                              dfi.departamento,
+                              nivel_educ_dept.poblacion_total AS poblacion_total       --Agrego la población total (útil luego para los gráficos)
+                            FROM dfi 
+                            INNER JOIN nivel_educ_dept 
+                              ON dfi.id_dept = nivel_educ_dept.id_departamento
+
+                            """) 
+dfi2 = consultaSQL2.df()   # Básicamente, la misma tabla que 'dfi', pero le agrego la cantidad de población por nivel educativo (haciendo JOIN entre 'dfi' y 'nivel_educ_dept').
+
+dfi2["Jardines"] = dfi2["jardin_infantes"] + dfi2["jardin_maternal"]    # Junto 'jardin_infantes' con 'jardin_maternal' para tener una sola columna de jardin (como se pide).
+dfi2["Poblacion Jardin"] = dfi2["Poblacion_infante"] + dfi2["Poblacion_maternal"]   # Junto también sus poblaciones.
+dfi2 = dfi2.drop(columns=["Poblacion_infante","Poblacion_maternal","jardin_infantes","jardin_maternal"])   # Elimino columnas innecesarias.
+dfi2 = dfi2[["Provincia","departamento","Jardines","Poblacion Jardin","primarias","poblacion_primaria","secundarios","poblacion_secundaria", "poblacion_total"]] # Ordeno las columnas.
+
+
+## Consulta ii.
+
+# En esta consulta se busca obtener la cantidad de BP fundadas desde 1950, agrupadas según su departamento.
+
+consulta_ii = duckdb.query(""" 
+                          SELECT dept.nombre_provincia as provincia, dept.nombre_departamento as departamento, count(BP.año_fundacion) AS cantidad_BP_fundadas_desde_1950     --Tomo los datos necesarios.
+
+                          FROM BP
+                          
+                          INNER JOIN dept                                           --Conecto la tabla BP con la de 'dept' para obtener la provincia y el nombre del departamento a partir de 'id_departamento' (clave en ambas tablas).
+                          ON dept.id_departamento = BP.id_departamento
+
+                          WHERE BP.año_fundacion >= 1950                            --Filtro según año.
+
+                          GROUP BY dept.nombre_provincia,dept.nombre_departamento
+
+                          ORDER BY dept.nombre_provincia, cantidad_BP_fundadas_desde_1950 DESC
+                          """) 
+dfii = consulta_ii.df()
+
+
+## Consulta iii.
+
+#En esta consulta se busca tomar la cantidad de BP y EE por departamento, junto con su población.
+
+aux = duckdb.query('''
+                   SELECT id_departamento, count(id_biblioteca) as cantBP 
+                   FROM BP
+                   GROUP BY id_departamento                   
+                   ''')
+cantBPdept = aux.df()   # Toma la cantidad de bibliotecas que hay por departamento.
+
+aux2 = duckdb.query('''
+                    SELECT EE.*
+                    FROM EE
+                    INNER JOIN EEcomunes 
+                        ON EE.id_establecimiento = EEcomunes.id_establecimiento                 
+                   ''')
+a2 = aux2.df()   # Me quedo con los EE comunes (como se pide) para luego contarlos según el departamento.
+
+aux2 = duckdb.query('''
+                    SELECT id_departamento, count(id_establecimiento) as cantEE
+                    FROM a2      
+                    GROUP BY id_departamento
+                   ''')
+cantEEdept = aux2.df()
+
+
+# Me voy armando tablas temporales para mantener el código encapsulado y organizado.
+
+consulta_iii = duckdb.query(""" 
+
+WITH tabla_temp AS(                                                            --Me quedo sólo con los establecimientos comunes.
+    SELECT *
+    FROM EE
+    INNER JOIN EEcomunes 
+        ON EE.id_establecimiento = EEcomunes.id_establecimiento
+),
+tabla_temp2 AS(                                                                --Conecto la tabla temporal anterior con la de 'bibliotecas', y me quedo con las columnas necesarias.         
+    SELECT 
+        tabla_temp.id_establecimiento,
+        tabla_temp.id_departamento,
+        BP.id_biblioteca
+    
+    FROM tabla_temp
+    
+    INNER JOIN BP
+        ON BP.id_departamento = tabla_temp.id_departamento
+)
+SELECT                                                                         --Conecto 'tabla_temp2' con las de 'dept', 'nivel_educ_dept', 'cantBPdept' y 'cantEEdept' para obtener los datos necesarios.
+
+    ANY_VALUE(dept.nombre_provincia) as Provincia,
+    dept.nombre_departamento as Departamento,
+    ANY_VALUE(cantBPdept.cantBP) as Cant_BP,
+    ANY_VALUE(cantEEdept.cantEE) as Cant_EE,
+    ANY_VALUE(nivel_educ_dept.poblacion_total) as Poblacion
+FROM tabla_temp2
+
+INNER JOIN dept
+ON dept.id_departamento = tabla_temp2.id_departamento
+
+INNER JOIN nivel_educ_dept
+    ON tabla_temp2.id_departamento = nivel_educ_dept.id_departamento
+
+INNER JOIN cantBPdept
+    ON cantBPdept.id_departamento = tabla_temp2.id_departamento
+    
+INNER JOIN cantEEdept
+    ON cantEEdept.id_departamento = tabla_temp2.id_departamento
+
+GROUP BY Departamento
+
+""") 
+
+dfiii = consulta_iii.df()
+
+
+## Consulta iv.
+
+# Lo que se busca en esta consulta es quedarte con el dominio de mail más frecuente para cada departamento según sus bibliotecas.
+# Me voy armando tablas temporales para mantener el código encapsulado y organizado.
+
+consulta_iv = duckdb.query(""" 
+
+WITH tabla_temporal AS (                                --Me armo una tabla temporal con los distintos dominios de mail no nulos que hay en cada departamento.
+  SELECT 
+    dept.nombre_provincia as provincia,
+    dept.nombre_departamento as departamento,
+    BP.dominio_email as mail 
+  FROM BP
+  INNER JOIN dept
+  ON dept.id_departamento = BP.id_departamento
+  WHERE mail IS NOT NULL
+),
+tabla_final AS (                                       --Tabla temporal para contar (y ordenar según) la cantidad de veces que aparece cada dominio de mail en cada departamento a partir de la tabla anterior.
+  SELECT 
+    ANY_VALUE(provincia) as provincia,
+    departamento,
+    mail,
+    COUNT(*) AS cantidad
+  FROM tabla_temporal
+  GROUP BY departamento, mail
+  ORDER BY cantidad DESC
+)
+SELECT                                                 --Consulta final que toma lo necesario de la tabla temporal anterior, el dominio de mail que más veces aparece en cada departamento, y agrupa por departamento.
+    ANY_VALUE(provincia) as Provincia, 
+    departamento as Departamento, 
+    ANY_VALUE(mail) as Dominio_mas_frecuente, 
+    MAX(cantidad) as max
+FROM tabla_final
+GROUP BY Departamento
+ORDER BY Departamento
+
+""") 
+dfiv = consulta_iv.df()
+
+dfiv = dfiv.drop(columns=["max"])   # Elimino la columna 'max' de la tabla final (me decía la cantidad de veces que aparecía cada dominio de mail por departamento).
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Paso los 'df' a tipo CSV, y los guardo para usarlos luego al hacer los graficos.
+'''
+
+dfi2.to_csv(consultas_path / "dfi.csv", index=False)
+dfii.to_csv(consultas_path / "dfii.csv", index=False)
+dfiii.to_csv(consultas_path / "dfiii.csv", index=False)
+dfiv.to_csv(consultas_path / "dfiv.csv", index=False)
+
+print("Todas los resultados de las consultas exportados exitosamente (en formato CSV) en la carpeta 'Consultas_SQL'")
+
+# Fin.
---
+++ TP Grupal 1/Scripts_y_DataSets/Graficos.py
@@ -0,0 +1,432 @@
+# Laboratorio de Datos - TP 1. 
+'''
+Integrantes -> Dorogov Cristina 
+            -> Pucciarelli Francisco Lautaro 
+            -> Salto Julian 
+               
+Visualización y Gráficos.
+'''
+
+'''
+Si aún no leyó el 'README', por favor leerlo.
+'''
+
+import duckdb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+import seaborn as sns
+from pathlib import Path
+
+'''
+Preparación de los Path a los diferentes archivos y carpeta de destino.
+'''
+
+base_path = Path(__file__).resolve().parent        # Obtener la ruta base del script actual. 
+modelo_path = base_path / 'TablasModelo'           # Ruta a la carpeta donde se encuentran los DataSets limpios. 
+consultas_path = base_path / 'ConsultasSQL'        # Ruta a la carpeta donde se van a guardar los DataSets limpios.
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Lectura de DataSets.
+'''
+
+EE_dept = pd.read_csv(consultas_path / "dfi.csv")
+BP_1950 = pd.read_csv(consultas_path / "dfii.csv")
+cantidad = pd.read_csv(consultas_path / "dfiii.csv")
+mails = pd.read_csv(consultas_path / "dfiv.csv")
+Departamentos= pd.read_csv(modelo_path / "departamentos.csv")
+
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Tarea Preliminar.
+'''
+
+# Tomo el promedio de CABA (agrupa las 15 comunas).
+
+index = EE_dept[EE_dept['departamento'] == 'CIUDAD DE BUENOS AIRES'].index
+
+EE_dept.iloc[index,2] = EE_dept.iloc[index,2] / 15
+EE_dept.iloc[index,3] = EE_dept.iloc[index,3] / 15
+EE_dept.iloc[index,4] = EE_dept.iloc[index,4] / 15
+EE_dept.iloc[index,5] = EE_dept.iloc[index,5] / 15 
+EE_dept.iloc[index,6] = EE_dept.iloc[index,6] / 15
+EE_dept.iloc[index,7] = EE_dept.iloc[index,7] / 15
+EE_dept.iloc[index,8] = EE_dept.iloc[index,8] / 15
+
+
+index2 = cantidad[cantidad['Departamento'] == 'CIUDAD DE BUENOS AIRES'].index
+
+cantidad.iloc[index2,2] = cantidad.iloc[index2,2] / 15 
+cantidad.iloc[index2,3] = cantidad.iloc[index2,3] / 15 
+cantidad.iloc[index2,4] = cantidad.iloc[index2,4] / 15 
+
+
+# ----------------------------------------------------------------------------------------------------------------------------------- #
+'''
+Gráficos.
+'''
+
+######### 1 ##########
+
+# Cant_BP por Provincia.
+consulta = duckdb.query(""" 
+                        SELECT Provincia, sum(Cant_BP) as Cant_BP
+                        FROM cantidad 
+                        GROUP BY Provincia
+                        ORDER BY Cant_BP
+                        """) 
+cantidad_prov = consulta.df()   # Sumo la cantidad de BP por provincia.
+
+# Cant_EE por Provincia.
+consulta2 = duckdb.query(""" 
+                         SELECT Provincia, sum(Cant_EE) as Cant_EE
+                         FROM cantidad 
+                         GROUP BY Provincia
+                         ORDER BY Cant_EE
+                         """) 
+cantidad_prov2 = consulta2.df()   # Sumo la cantidad de EE por provincia.
+
+# Grafico con esas tablas.
+fig, ax = plt.subplots(figsize=(12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+cantidad_prov.plot.bar(x = 'Provincia',
+                       y = 'Cant_BP',
+                       ax = ax,
+                       color = 'b',
+                       legend = False)
+
+# Ajustes.
+ax.set_title('Cantidad Total de BP por Provincia')
+ax.set_xlabel('Provincias', fontsize='medium')
+ax.set_ylabel('BP', fontsize='medium')
+ax.set_ylim(0, 550)
+
+# Coloco los valores arriba de cada barra.
+ax.bar_label(ax.containers[0], fontsize=6, padding=2)
+
+# Roto etiquetas en 'x' para que los nombres de las provincias queden claros.
+plt.xticks(rotation=45, ha='right', fontsize=8)
+plt.tight_layout()
+plt.show()
+
+
+## Mismo grafico que el anterior pero con EE.
+fig, ax = plt.subplots(figsize=(12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+cantidad_prov2.plot.bar(x = 'Provincia',
+                        y = 'Cant_EE',
+                        ax = ax,
+                        color = 'b',
+                        legend = False)
+
+# Ajustes.
+ax.set_title('Cantidad total de EE por Provincia')
+ax.set_xlabel('Provincias', fontsize='medium')
+ax.set_ylabel('EE', fontsize='medium')
+ax.set_ylim(0, 17000)
+
+# Coloco los valores arriba de cada barra.
+ax.bar_label(ax.containers[0], fontsize=6, padding=2)
+
+# Roto etiquetas en 'x'.
+plt.xticks(rotation=45, ha='right', fontsize=8)
+plt.tight_layout()
+plt.show()
+
+
+## Grafico extra -> Junto ambos gráficos anteriores para poder realizar una comparación mas eficiente.
+
+merge = duckdb.query(""" 
+                     SELECT cantidad_prov.Provincia, cantidad_prov.Cant_BP, cantidad_prov2.Cant_EE
+                     FROM cantidad_prov
+                     INNER JOIN cantidad_prov2
+                     ON cantidad_prov.Provincia = cantidad_prov2.Provincia 
+                     ORDER BY cantidad_prov2.Cant_EE
+                     """) 
+cantidad_merge = merge.df()   # Mergeo las 2 tablas que usé para los gráficos anteriores.
+
+# Grafico.
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+# Aplico dos ejes Y, que tome cada uno un rango diferente (uno para BP y otro para EE).
+cantidad_merge.set_index('Provincia')[['Cant_EE', 'Cant_BP']].plot(kind='bar',
+                                                                   ax=ax,
+                                                                   secondary_y='Cant_BP',   # 2do eje.
+                                                                   color=['red', 'blue'])
+
+# Ajustes.
+ax.set_title('EE y BP por Provincia')
+ax.set_xlabel('Provincias', fontsize='medium')
+ax.set_ylabel('cantidad EE', fontsize='medium')
+ax.right_ax.set_ylabel('cantidad BP', fontsize='medium')
+ax.set_ylim(0, 17500)
+
+# Rotar etiquetas en 'x'.
+plt.xticks(rotation=45, ha='right', fontsize=8)
+plt.tight_layout()
+plt.show()
+
+
+### Extras -> Graficos iguales a los 2 primeros pero normalizados.
+
+nuevo = duckdb.query(""" 
+                     SELECT Provincia, sum(Cant_BP) as Cant_BP, sum(Cant_EE) as Cant_EE, sum(Poblacion) as Poblacion
+                     FROM cantidad
+                     GROUP BY Provincia 
+                     """) 
+cantidad_nuevo = nuevo.df()   # Sumo la cantidad de BP, EE y población por provincia, para luego normalizar.
+
+cantidad_nuevo["Cant_EE"] = cantidad_nuevo["Cant_EE"]/cantidad_nuevo["Poblacion"] *1000   # Normalizo (cantidad de BP y EE cada 1000 habitantes).
+cantidad_nuevo["Cant_BP"] = cantidad_nuevo["Cant_BP"]/cantidad_nuevo["Poblacion"] *1000
+cantidad_nuevo = cantidad_nuevo.sort_values(by='Cant_EE')
+
+## Grafico con EE.
+fig, ax = plt.subplots(figsize=(12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+cantidad_nuevo.plot.bar(x = 'Provincia',
+                        y = 'Cant_EE',
+                        ax = ax,
+                        color = 'b',
+                        legend = False)
+
+# Ajustes.
+ax.set_title('Cantidad total de EE por Provincia (Normalizado)')
+ax.set_xlabel('Provincias', fontsize='medium')
+ax.set_ylabel('EE', fontsize='medium')
+ax.set_ylim(0, 3)
+
+# Coloco los valores arriba de cada barra.
+ax.bar_label(ax.containers[0], fmt = '%.3f', fontsize = 6, padding = 2)   # El fmt='%.3f' sirve para tomar solo 3 decimales. 
+
+# Roto etiquetas en 'x'.
+plt.xticks(rotation = 45, ha = 'right', fontsize = 8)
+plt.tight_layout()
+plt.show()
+
+## Ahora grafico pero con BP.
+cantidad_nuevo = cantidad_nuevo.sort_values(by = 'Cant_BP')
+
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+cantidad_nuevo.plot.bar(x = 'Provincia',
+                        y = 'Cant_BP',
+                        ax = ax,
+                        color = 'b',
+                        legend = False)
+
+# Ajustes.
+ax.set_title('Cantidad total de BP por Provincia (Normalizado)')
+ax.set_xlabel('Provincias', fontsize = 'medium')
+ax.set_ylabel('BP', fontsize = 'medium')
+ax.set_ylim(0, 0.35)
+
+# Coloco los valores arriba de cada barra.
+ax.bar_label(ax.containers[0], fmt = '%.3f', fontsize = 6, padding = 2)
+
+# Roto etiquetas en 'x'.
+plt.xticks(rotation = 45, ha = 'right', fontsize = 8)
+plt.tight_layout()
+plt.show()
+
+
+############# 2 ############### 
+
+# Grafico cada nivel educativo en graficos separados para que se puedan comprender mejor (sacar mejores conclusiones).
+
+## JARDINES.
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+EE_dept.plot.scatter(x = 'poblacion_total',
+                     y = 'Jardines',
+                     ax = ax,
+                     color = 'b',
+                     legend = False)
+
+ax.set_xscale('log')   # Aplico escala logarítmica para mayor separación entre los puntos -> Gráfico más comprensible.
+
+# Ajustes.
+ax.set_title('Cantidad de Jardines vs Población')
+ax.set_xlabel('Poblacion (escala log)', fontsize = 'medium')
+ax.set_ylabel('Jardines', fontsize = 'medium')
+ax.set_ylim(0, 400)
+
+## PRIMARIAS.
+
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+EE_dept.plot.scatter(x = 'poblacion_total',
+                     y = 'primarias',
+                     ax = ax,
+                     color = 'b',
+                     legend = False)
+ax.set_xscale('log')
+
+# Ajustes.
+ax.set_title('Cantidad de Primarias vs Población')
+ax.set_xlabel('Poblacion (escala log)', fontsize = 'medium')
+ax.set_ylabel('Primarias', fontsize = 'medium')
+ax.set_ylim(0, 450)
+
+
+## SECUNDARIOS.
+
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+EE_dept.plot.scatter(x = 'poblacion_total',
+                     y = 'secundarios',
+                     ax = ax,
+                     color = 'b',
+                     legend = False)
+ax.set_xscale('log')
+
+# Ajustes.
+ax.set_title('Cantidad de Secundarias vs Población')
+ax.set_xlabel('Poblacion (escala log)', fontsize = 'medium')
+ax.set_ylabel('Secundarios', fontsize = 'medium')
+ax.set_ylim(0, 500)
+
+
+## Bibliotecas por Poblacion.
+# Grafico la evolucion de la cantidad de BP a medida que aumenta la población (cada punto representa un departamento).
+
+fig, ax = plt.subplots(figsize = (12,6))
+plt.rcParams['font.family'] = 'sans-serif'
+
+cantidad.plot.scatter(x = 'Poblacion',
+                      y = 'Cant_BP',
+                      ax = ax,
+                      color = 'b',
+                      legend = False)
+ax.set_xscale('log')
+
+# Ajustes.
+ax.set_title('Cantidad de Bibliotecas vs Población')
+ax.set_xlabel('Poblacion (escala log)', fontsize = 'medium')
+ax.set_ylabel('Bibliotecas Públicas', fontsize = 'medium')
+ax.set_ylim(0, 60)
+
+
+############## 3 ###############
+
+medianas = cantidad.groupby('Provincia')['Cant_EE'].median().sort_values()   # Calcula la mediana de cada provincia (agrupadas) con respecto a la cantidad de EE por departamento.
+orden_provincias = medianas.index.tolist()   # Ordeno la tabla segun la mediana de cada provincia.
+
+fig, ax = plt.subplots(figsize = (10, 8))
+
+plt.figure(figsize = (10, 8))
+sns.boxplot(data = cantidad,
+            x = 'Cant_EE',
+            y = 'Provincia',
+            order = orden_provincias,
+            palette = 'Set2',
+            ax = ax)
+
+# Títulos y ejes.
+ax.set_title('Cantidad de EE por Departamento segun Provincia')
+ax.set_xlabel('Cantidad de EE', fontsize = 'medium')
+ax.set_ylabel('Provincias', fontsize = 'medium')
+ax.set_xlim(0, 1800) 
+
+plt.tight_layout()
+plt.show()
+
+
+## Mismo grafico pero con BP.
+
+medianas2 = cantidad.groupby('Provincia')['Cant_BP'].median().sort_values()   # Calcula la mediana de cada provincia (agrupadas) con respecto a la cantidad de BP por departamento.
+orden_provincias2 = medianas2.index.tolist()
+
+fig, ax = plt.subplots(figsize = (10, 8))
+
+plt.figure(figsize = (10, 8))
+sns.boxplot(data = cantidad,
+            x = 'Cant_BP',
+            y = 'Provincia',
+            order = orden_provincias2,
+            palette = 'Set2',
+            ax = ax)
+
+# Títulos y ejes.
+ax.set_title('Cantidad de BP por Departamento segun Provincia')
+ax.set_xlabel('Cantidad de BP', fontsize = 'medium')
+ax.set_ylabel('Provincias', fontsize = 'medium')
+ax.set_xlim(0, 60) 
+
+plt.tight_layout()
+plt.show()
+
+
+############## 4 ###############
+
+cantidad['BP_per_1000'] = cantidad['Cant_BP'] / cantidad['Poblacion'] * 1000   # Calculo la cantidad de BP y EE cada 1000 habitantes, por departamento (para normalizar).
+cantidad['EE_per_1000'] = cantidad['Cant_EE'] / cantidad['Poblacion'] * 1000
+
+plt.figure(figsize = (8, 6))
+ax = sns.scatterplot(data = cantidad,
+                     x = 'BP_per_1000',
+                     y = 'EE_per_1000',
+                     hue = 'Provincia',   # Color de cada punto según a la provincia a la que pretenece ese departamento.
+                     palette = 'tab10',
+                     s = 60,   # Tamaño de los puntos.
+                     alpha = 0.8,   # Transparencia de cada punto (para poder hacer visible la superposición de valores).
+                     legend = 'brief')
+ax.set_xscale('log')
+ax.set_yscale('log')
+ax.set_ylabel('Cantidad de EE (escala log)', fontsize='medium')
+ax.set_xlabel('Cantidad de BP (escala log)', fontsize='medium')
+ax.legend(title = 'Provincia', bbox_to_anchor = (1, 1), loc = 'upper left')   # Agrego la leyenda fuera del gráfico con las provincias y sus respectivos colores.
+plt.title('Cantidad de BP y EE cada 1000 habitantes por departamento', fontsize = 13)
+plt.tight_layout()
+plt.show()
+
+
+############# GRÁFICOS EXTRAS ###############
+
+#### E-1.
+
+# Grafico un HEATMAP que nos pueda ayudar a relacionar EE con BP.
+
+extra_2 = duckdb.query(""" 
+                       SELECT
+                           Provincia,
+                           sum(Cant_BP) as Cant_BP,
+                           sum(Cant_EE) as Cant_EE,
+                           sum(Poblacion) as poblacionTot
+                       FROM cantidad
+                       GROUP BY Provincia
+                       ORDER BY Cant_EE
+                       """) 
+extra2 = extra_2.df()   # Tomo la cantidad de BP y EE por provincia, y su poblacion total (para despues normalizar).
+
+extra2["Cant_BP"] = extra2["Cant_BP"]/extra2["poblacionTot"] *1000   # Normalizo la tabla.
+extra2["Cant_EE"] = extra2["Cant_EE"]/extra2["poblacionTot"] *1000
+extra2 = extra2.drop(columns="poblacionTot")
+
+# Me aseguro de tener solo columnas numericas (de cantidad de BP y EE) y a la provincia como índice (para realizar el heatmap).
+extra2_clust = extra2.set_index("Provincia")[["Cant_BP", "Cant_EE"]] 
+
+a = sns.clustermap(extra2_clust,
+                   col_cluster = False,   # Para que no se agrupen las columnas, si no las filas (provincias).
+                   method = "single",   # Impone un orden a las provincias (relaciona provincias para formar cadenas).      
+                   cmap = "Blues",
+                   standard_scale = 1   # Para comparar a las provincias entre sí.
+                   )
+
+a.fig.suptitle('Relación entre provincias\n(sobre la cantidad de EE y BP)', y = 0.90, fontsize = 16)
+
+plt.show()
+
+# Fin.
